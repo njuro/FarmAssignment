55b4a3e6aa60941ff31933862a04a9560dad6637
njuro <juraj.noge@gmail.com>
2020-08-26 17:16:07 +0200

    Initial commit

---
+++ src/main/kotlin/cz/cleverfarm/interview/farmassignment/FarmAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package cz.cleverfarm.interview.farmassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FarmAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<FarmAssignmentApplication>(*args)
+}
